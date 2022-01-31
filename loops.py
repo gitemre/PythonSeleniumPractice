1b37823b4bbb272892cad93da8d58f905e7422d5
gitemre <emreozturk333@aof.anadolu.edu.tr>
2022-01-31 17:07:29 +0300

    loops.py

---
+++ loops.py
@@ -0,0 +1,30 @@
+greeting = "Good Morning"
+
+if greeting == "Good Morning":
+    print("Condition matches")
+    print("second line")
+else:
+    print("condition do not match")
+
+print("if else condition code is completed")
+
+# for loop
+
+obj = [2, 3, 5, 7, 9]
+for i in obj:
+    print(i * 2)
+
+# sum of Firts Natural numbers 1+2+3+4+5 = 15
+summation = 0
+for j in range(1, 6):
+    summation = summation + j
+print(summation)
+
+print("***********************************************************")
+for k in range(1,10,5):
+    print(k)
+
+
+print("***********************************************************")
+for m in range(10):
+    print(m)

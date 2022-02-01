e5b1d73271b5ef828874a96d64a5ab3904ee186b
gitemre <emreozturk333@aof.anadolu.edu.tr>
2022-02-01 12:27:05 +0300

    dailypractice

---
+++ StringsDemo.py
@@ -0,0 +1,20 @@
+str = "Öztürk.emreozturk123"
+str1 = "Deneme123"
+str3 = "Öztürkemreozturk"
+
+print(str[1])
+
+print(str[0:5])
+
+print(str+str1)
+
+print(str3 in str)
+
+var = str.split(".")
+print(var)
+print(var[0])
+
+str4 = " great "
+print(str4.strip())
+print(str4.lstrip())
+print(str4.rstrip())
